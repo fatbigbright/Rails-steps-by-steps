a80b0edcc9af7f9a4d85eb8940a9ccd1cdb1568f
Chang,Liang <bigbrightf@gmail.com>
2013-11-25 19:23:27 +0800

    1. Review of demo yesterday, create site by using ruby1.9.3@rails3.2.12 2. Integrate CSS exercise today to action welcome#say_hello

---
+++ demo20131125/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo20131125::Application.config.secret_token = 'daadffd56578d86eb391b682e32d2d22efa9afb3d2347048449d07a70d5012cef7c3d0656db7765f02fb77f51af87c107e9612118fb2e7ed923403f66a58ead7'
